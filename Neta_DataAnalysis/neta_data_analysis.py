962ebc8dd1b939076f0385a01aa20e0c76943d71
anatshk <anat.shk@gmail.com>
2018-01-19 13:17:38 +0200

    wip neta_data_analysis

--- Neta_DataAnalysis/neta_data_analysis.py
+++ Neta_DataAnalysis/neta_data_analysis.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from os import path
+from utils import save_to_pkl, load_from_pkl
+
 """
 For each row in Laproscopy DB (experiment.xlsx), find 2 rows in Control DB (contron.xlsx).
 
@@ -13,9 +16,10 @@ Control sample is suitable if:
 Add Columns in Control:
 1. Calculate maternal age during birth.
 2. Number of fetuses per birth (duplicate row = more than one fetus).
-3. In experiment (Lapro) table - a single patient can undergo several procedures. 
-If all procedures are within scope of same pregnancy - treat them as single row and only find matches from control once.
 
+Remove duplicates in Experiments: 
+1. In experiment (Lapro) table - a single patient can undergo several procedures. 
+   If all procedures are within scope of same pregnancy - treat them as single row and find matches from control once.
 
 Data Validation:
 1. Rows in experiment should be removed from control.
@@ -26,3 +30,63 @@ Other:
   Comparison should be contains and not exact. 
   In 'experiment' sheet in experiment.xlsx, there is a list of the selected rows.
 """
+
+# load files
+pkl_pth = 'temp.pkl'
+if path.exists(pkl_pth):
+    control, experiment = load_from_pkl(pkl_pth).values()
+else:
+    control = pd.read_excel('control.xlsx').dropna()
+    experiment = pd.read_excel('experiment.xlsx', sheet_name='experiment').dropna()
+    save_to_pkl(pkl_pth, control=control, experiment=experiment)
+
+
+def create_new_index(df):
+    # remove rows without parity
+    df.dropna(subset=['parity'], inplace=True)
+
+    # create a new index of case, id, parity
+    def new_index(row):
+        return row.case + '_' + row.id + '_' + str(row.parity)
+    df['new_index'] = df.apply(new_index, axis=1)
+    return df
+
+
+def arrange_control(df):
+    """
+    Arranges the control file.
+    :param df: Pandas frame
+    :return: Pandas frame
+    """
+
+    df = create_new_index(df)
+
+    # new column - count number of fetuses (= duplicate rows) - find duplicates and count
+    duplicated = df.loc[df.duplicated(keep=False)]
+    counted_multi_fetuses = duplicated.groupby('new_index').count()['id']
+
+    def num_fetuses(row):
+        return counted_multi_fetuses[row.new_index] if row.new_index in counted_multi_fetuses else 1
+    df['num_fetuses'] = df.apply(num_fetuses, axis=1)
+
+    # TODO: calculate maternal age at birth? if columns are not timestamps - convert
+
+    return df
+
+
+def arrange_experiment(df):
+    """
+    Arranges experiment data - dropping rows without parity,
+    :param df: Pandas frame, experiment
+    :return: Pandas frame
+    """
+
+    df = create_new_index(df)
+    
+
+# clean_control = arrange_control(control.copy())
+clean_experiment = arrange_experiment(experiment.copy())
+
+
+
+a = 5
