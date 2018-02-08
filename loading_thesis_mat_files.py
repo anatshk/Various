baddc245f003775ac2c1bbe09afcf520f2ec2529
anatshk <anat.shk@gmail.com>
2018-02-08 13:02:29 +0200

    load mat files

---
+++ loading_thesis_mat_files.py
@@ -0,0 +1,7 @@
+from scipy.io import loadmat
+
+# pth = r'C:\Users\Anat\Google Drive\Uni\MatlabCode\Thesis Code Summary\GT\Labeled DBs for CNN BoW\Balanced-TrainTest-3-DBs-Increased1-Centered\BinaryGrayscaleDBLabels-Test.mat'
+pth = r'C:\Users\Anat\Dropbox\MitosisForGal\SequenceDB with GT\Mov1-Frames-1-1340-2014-08-26-1108\data.mat'
+mat = loadmat(pth)
+
+a = 5
