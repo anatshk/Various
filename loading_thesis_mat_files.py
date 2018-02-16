4f1a10093e2232a4ebc2bcf90702c5dd5b1bca91
anatshk <anat.shk@gmail.com>
2018-02-16 20:11:48 +0200

    file to load mat files

--- loading_thesis_mat_files.py
+++ loading_thesis_mat_files.py
@@ -1,7 +1,9 @@
 from scipy.io import loadmat
+import numpy as np
 
 # pth = r'C:\Users\Anat\Google Drive\Uni\MatlabCode\Thesis Code Summary\GT\Labeled DBs for CNN BoW\Balanced-TrainTest-3-DBs-Increased1-Centered\BinaryGrayscaleDBLabels-Test.mat'
-pth = r'C:\Users\Anat\Dropbox\MitosisForGal\SequenceDB with GT\Mov1-Frames-1-1340-2014-08-26-1108\data.mat'
+# pth = r'C:\Users\Anat\Dropbox\MitosisForGal\SequenceDB with GT\Mov1-Frames-1-1340-2014-08-26-1108\data.mat'
+pth = r"C:\Users\Anat\Dropbox\MitosisForGal\Thesis Code Summary\MatConvNet\MNIST-MITOSISVAL-2015-01-22.mat"
 mat = loadmat(pth)
 
 a = 5
